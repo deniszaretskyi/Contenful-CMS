90377ffde44d511f7b1e90226c818da31b90a238
Denys Zaretskyi <denis.zaretskyi@icloud.com>
2024-12-12 16:06:18 +0200

    Initial commit

---
+++ src/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="heroText">
+        <h1>Contentful CMS</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+      </div>
+      <div className="heroImage"></div>
+    </section>
+  );
+};
+
+export default Hero;
